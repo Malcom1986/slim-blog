ec0574eedb6d5b5d6d41ef34d5f63f7e8a916087
Maksim Litvinov <litvinovmksm@gmail.com>
2020-09-16 17:46:31 +0300

    add dynamic route

--- public/index.php
+++ public/index.php
@@ -12,12 +12,29 @@ $app->get('/', function ($request, $response) {
     return $response;
 });
 
+
+$app->get('/users/{id}', function ($request, $response, $args) {
+	$id = $args['id'];
+    return $response->write("Users id is {$id}");
+});
+
 $app->get('/users', function ($request, $response) {
     return $response->write('GET /users');
 });
 
+
+
 $app->post('/users', function ($request, $response) {
     return $response->write('POST /users');
+});
+
+$app->get('/headers', function ($request, $response) {
+	$headers = json_encode($request->getHeaders(), JSON_PRETTY_PRINT);
+	return $response->write($headers);
+});
+
+$app->post('/articles', function ($request, $response) {
+	return $response->withStatus(302);
 });
 
 $app->run();
