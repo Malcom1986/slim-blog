5b06edfb935b6e635d5813f43c860a2711e33219
Maksim Litvinov <litvinovmksm@gmail.com>
2020-09-29 17:36:05 +0300

    fix linter errors

--- src/Repository.php
+++ src/Repository.php
@@ -1,29 +1,31 @@
 <?php
 
+namespace App;
+
 class Repository
 {
-	const PATH = __DIR__ . '/../repository/users';
+    private const PATH = __DIR__ . '/../repository/users';
 
-	public function add($data)
-	{
-		$id = uniqid();
-		$data['id'] = $id;
-		$encodedData = json_encode($data) . "\n";
+    public function add($data)
+    {
+        $id = uniqid();
+        $data['id'] = $id;
+        $encodedData = json_encode($data) . "\n";
         file_put_contents(self::PATH, $encodedData, FILE_APPEND);
-	}
+    }
 
-	public function all()
-	{
-		$json = file_get_contents(self::PATH);
-		$dataStrings = explode("\n", $json);
-		$data = array_map(fn ($string) => json_decode($string), $dataStrings);
-		return $data;
-	}
+    public function all()
+    {
+        $json = file_get_contents(self::PATH);
+        $dataStrings = explode("\n", $json);
+        $data = array_map(fn ($string) => json_decode($string), $dataStrings);
+        return $data;
+    }
 
-	public function get($id)
-	{
-		$allData = $this->all();
-		$data = array_filter($allData, fn ($item) => $item->id === $id);
-		return $data;
-	}
+    public function get($id)
+    {
+        $allData = $this->all();
+        $data = array_filter($allData, fn ($item) => $item->id === $id);
+        return $data;
+    }
 }
--- src/Validator.php
+++ src/Validator.php
@@ -4,17 +4,17 @@ namespace App;
 
 class Validator
 {
-	public function validate(array $user)
-	{
-		$errors = [];
-		foreach ($user as $key => $value) {
-			if (empty($user[$key])) {
-				$errors[$key] = 'Can not be blank';
-			}
-		}
-		if ($user['password'] !== $user['passwordConfirmation']) {
-			$errors['passwordConfirmation'] = 'passwords must match';
-		}
-		return $errors;
-	}
+    public function validate(array $user)
+    {
+        $errors = [];
+        foreach ($user as $key => $value) {
+            if (empty($user[$key])) {
+                $errors[$key] = 'Can not be blank';
+            }
+        }
+        if ($user['password'] !== $user['passwordConfirmation']) {
+            $errors['passwordConfirmation'] = 'passwords must match';
+        }
+        return $errors;
+    }
 }
